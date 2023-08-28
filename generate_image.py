f4ac18fb2a5ae25180a62d63f920915d9ee0d705
Jason Hall <jasonbrianhall@gmail.com>
2023-08-28 14:32:19 -0400

    Command arguments

--- generate_image.py
+++ generate_image.py
@@ -2,7 +2,7 @@
 
 import torch
 from diffusers import StableDiffusionPipeline
-import os
+import os, sys
 import threading
 import math
 from time import sleep
@@ -23,95 +23,133 @@ def worker(device, prompt, output_file, finish_event, iterations=20):
 	
 	finish_event.set()	# Signal that the thread has finished
 
-CPUENABLED=True
+def main():
 
-# Prompt user for input
-prompt = input("Enter the text prompt: ")
+	args = sys.argv[1:]
 
-# Define output file base name
-output_base_name = input("Enter the output base name: ")
+	iterations = -1
+	prompt = None
+	CPUEnabled = False
+	numberofversions = -1
+	output_base_name = None
+	useOpenGL=False
 
-numberofversions = int(input("Enter the number of versions: "))
-
-iterations = int(input("Enter the number of iterations: "))
-
-versioncount=0
-
-devices = []
-
-for i in range(torch.cuda.device_count()):
-	gpu = torch.cuda.get_device_properties(i)
-	if gpu.total_memory > 8 * 1024 ** 3:  # Check if GPU has more than 8 GB of RAM
-		if versioncount<numberofversions:
-			versioncount+=1
-			devices.append(f"cuda:{i}")
-
-if CPUENABLED==True:
-	num_cpus=os.cpu_count()
-	num_threads = max(1, num_cpus // 16)
-	if num_threads<=0:
-		num_threads=1
-	for x in range(0, num_threads):
-		if versioncount<numberofversions:
-			versioncount+=1
-			devices.append("cpu")
-
-# Create a finish event for each device
-finish_events = {device: threading.Event() for device in devices}
-
-queue1 = Queue()
-queue2 = Queue()
-
-threads={}
-for device in devices:
-	threads[device]=0
+	for i in range(0, len(args)):
+		if args[i] == '-i':
+			iterations = int(args[i+1])
+		elif args[i] == '-p':  
+			prompt = args[i+1]
+		elif args[i] == '-c':
+			CPUEnabled = True 
+		elif args[i] == '-v':
+			numberofversions = int(args[i+1])
+		elif args[i] == '-o':  
+			output_base_name = args[i+1]
+			print("Setting output_base_name")
+		elif args[i] == '-O':  
+			useOpenGL=True
+			print("Setting OpenGL")
+		elif args[i] == '-h':  
+			print(sys.argv[0] + " -i iteraions -p prompt -c (CPU Enabled) -v numberoversions -o outputbasename -O (useopengl instead of CUDA) -h (help)")
+			sys.exit(1)    
 
 
-versionNumber=0
-for device in devices:
-	versionNumber+=1
-	temp=threads[device]
-	threads[device]+=1
-	output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}.png"
-	if os.path.exists(output_file):
-		# Generate 8 random characters
-		while os.path.exists(output_file):
-			random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
-			output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}-{random_string}.png"
-	thread = threading.Thread(name=device, target=worker, args=(device, prompt, output_file, finish_events[device], iterations))
-	queue1.put(thread)
-	thread.start()
-	print("\n\nStarting version", versionNumber, "with device", device, "and prompt", prompt, "and outputbasename", output_base_name)
+	# Prompt user for input
+	if prompt==None:
+		prompt = input("Enter the text prompt: ")
 
-while not queue1.empty():
-	while not queue1.empty():
-		thread=queue1.get()
-		if not thread.is_alive():
-			thread.join()
-			if versionNumber<numberofversions:
-				versionNumber+=1
-				device=thread.name
-				print("\n\nStarting version", versionNumber, "with device", device, "and prompt", prompt, "and outputbasename", output_base_name)
-				temp=threads[device]
-				threads[device]+=1
-				output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}.png"
-				if os.path.exists(output_file):
-					while os.path.exists(output_file):
-						# Generate 8 random characters
-						random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
-						output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}-{random_string}.png"
-				thread = threading.Thread(name=device, target=worker, args=(device, prompt, output_file, finish_events[device], iterations))
-				queue2.put(thread)
-				thread.start()
-		else:
-			queue2.put(thread)
-		sleep(1)	
-	while not queue2.empty():
-		thread=queue2.get()
+	# Define output file base name
+	if output_base_name==None:
+		output_base_name = input("Enter the output base name: ")
+
+	if numberofversions<=0:
+		numberofversions = int(input("Enter the number of versions: "))
+
+	if iterations<=0:
+		iterations = int(input("Enter the number of iterations: "))
+
+	versioncount=0
+
+	devices = []
+
+	for i in range(torch.cuda.device_count()):
+		gpu = torch.cuda.get_device_properties(i)
+		if gpu.total_memory > 8 * 1024 ** 3:  # Check if GPU has more than 8 GB of RAM
+			if versioncount<numberofversions:
+				versioncount+=1
+				if useOpenGL==False:
+					devices.append(f"cuda:{i}")
+				else:
+					devices.append(f"opengl:{i}")
+
+	if CPUEnabled==True:
+		num_cpus=os.cpu_count()
+		num_threads = max(1, num_cpus // 16)
+		if num_threads<=0:
+			num_threads=1
+		for x in range(0, num_threads):
+			if versioncount<numberofversions:
+				versioncount+=1
+				devices.append("cpu")
+
+	# Create a finish event for each device
+	finish_events = {device: threading.Event() for device in devices}
+
+	queue1 = Queue()
+	queue2 = Queue()
+
+	threads={}
+	for device in devices:
+		threads[device]=0
+
+
+	versionNumber=0
+	for device in devices:
+		versionNumber+=1
+		temp=threads[device]
+		threads[device]+=1
+		output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}.png"
+		if os.path.exists(output_file):
+			# Generate 8 random characters
+			while os.path.exists(output_file):
+				random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+				output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}-{random_string}.png"
+		thread = threading.Thread(name=device, target=worker, args=(device, prompt, output_file, finish_events[device], iterations))
 		queue1.put(thread)
+		thread.start()
+		print("\n\nStarting version", versionNumber, "with device", device, "and prompt", prompt, "and outputbasename", output_base_name)
+
+	while not queue1.empty():
+		while not queue1.empty():
+			thread=queue1.get()
+			if not thread.is_alive():
+				thread.join()
+				if versionNumber<numberofversions:
+					versionNumber+=1
+					device=thread.name
+					print("\n\nStarting version", versionNumber, "with device", device, "and prompt", prompt, "and outputbasename", output_base_name)
+					temp=threads[device]
+					threads[device]+=1
+					output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}.png"
+					if os.path.exists(output_file):
+						while os.path.exists(output_file):
+							# Generate 8 random characters
+							random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+							output_file = f"{output_base_name}-{device.replace(':', '')}-{temp}-{random_string}.png"
+					thread = threading.Thread(name=device, target=worker, args=(device, prompt, output_file, finish_events[device], iterations))
+					queue2.put(thread)
+					thread.start()
+			else:
+				queue2.put(thread)
+			sleep(1)	
+		while not queue2.empty():
+			thread=queue2.get()
+			queue1.put(thread)
+			
+if __name__ == '__main__':
+    main()
+			
+			
 		
-		
-		
-	
 
 
