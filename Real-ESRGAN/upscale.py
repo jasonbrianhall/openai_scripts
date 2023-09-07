3ee525b16a5a25419838d693234f2b162f733b07
Jason Hall <jasonbrianhall@gmail.com>
2023-09-07 13:50:50 -0400

    Upscale image

--- Real-ESRGAN/upscale.py
+++ Real-ESRGAN/upscale.py
@@ -1,25 +1,19 @@
 import torch
-from PIL import Image 
-from tqdm import tqdm
+from PIL import Image
+import numpy as np
 from RealESRGAN import RealESRGAN
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load model 
 model = RealESRGAN(device, scale=4)
-model.load_weights('weights/RealESRGAN_x4.pth')
+model.load_weights('weights/RealESRGAN_x4.pth', download=True)
 
-# Load image
-img = Image.open('image.png').convert('RGB')
+filename=input("Filename to upscale:  ")
 
-# Upscale image
-with torch.no_grad():
-  pbar = tqdm(total=2, desc='Upscaling')
-  sr_img = None
-  for i in range(2):   
-    pbar.update(1)
-    sr_img = model.predict(img)  
-  pbar.close()
+image = Image.open(filename).convert('RGB')
 
-# Save image
-sr_img.save('upscaled.png')
+sr_image = model.predict(image)
+
+filename_split=filename.split(".")[0]
+
+sr_image.save(filename_split + "_upscaled.png")
