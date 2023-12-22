1c005ac3798aa47fa6e045560fa9991562a82926
Jason Hall <jasonbrianhall@gmail.com>
2023-12-22 09:50:05 -0500

    Pix 2 pix

---
+++ pix2pix.py
@@ -0,0 +1,19 @@
+import PIL
+import torch
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+
+model_id = "timbrooks/instruct-pix2pix"
+pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+pipe.to("cuda")
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+
+input_file = input("Enter image filename: ")
+prompt = input("Enter prompt: ")
+output_file = input("Enter output filename: ")
+
+image = PIL.Image.open(input_file)
+image = PIL.ImageOps.exif_transpose(image)
+image = image.convert("RGB")
+
+images = pipe(prompt, image=image, num_inference_steps=10, image_guidance_scale=1).images
+images[0].save(output_file)
